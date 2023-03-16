9b03ddb998d1b186a7d9a062b2a146e068cbb924
Yaroslav Lisitsyn <94788726+rene-reynard@users.noreply.github.com>
2023-03-16 23:12:26 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Workouts from "./components/Workouts";
+
+function App() {
+  return (
+    <div className="App">
+      <Workouts />
+    </div>
+  );
+}
+
+export default App;
